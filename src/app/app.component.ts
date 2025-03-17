c0f0cb31b0b3a6ea1b963b0170999967ac892d3d
Simert,Mechu <Mechu.Simert@adesso.de>
2025-03-17 08:55:26 +0100

    last state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,59 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'website';
+  inputMatch: boolean = false;
+  timeOfDay!: string;
+  magicSubmission!: FormGroup;
+  currentDate = new Date();
+  currentHour = this.currentDate.getHours();  // Gibt die Stunde im 24-Stunden-Format zurück
+
+  constructor(private formBuilder: FormBuilder) {
+    this.magicSubmission = this.formBuilder.group({
+      magicWord: ['']
+    });
+    // Beobachte Änderungen an 'magicWord' und prüfe die Eingabe
+
+  }
+
+  ngOnInit() {
+    console.log("init");
+    this.setThemeDependingOnTime();
+  }
+
+  ngOnChange(changes: SimpleChanges): void {
+    if (changes['magicWord']) {
+      const magicWord = changes['magicWord'].currentValue;
+      console.log(magicWord)
+    } else {
+      return;
+    }
+  }
+
+  // Methode zum Überprüfen des Inputs
+  checkInput(value: string) {
+    if (value.trim().toLowerCase() === 'hallo') {
+      return this.inputMatch = true;
+    } else {
+      return this.inputMatch = false;
+    }
+  }
+
+  checkTime() {
+    if (this.currentHour >= 6 && this.currentHour < 12) {
+      return this.timeOfDay = 'morning';
+    } else if (this.currentHour >= 12 && this.currentHour < 18) {
+      return this.timeOfDay = 'noon';
+    } else if (this.currentHour >= 18 && this.currentHour < 21) {
+      return this.timeOfDay = 'evening';
+    } else {
+      return this.timeOfDay = 'night';
+    }
+  }
+
+  setThemeDependingOnTime() {
+    return document.documentElement.setAttribute('data-theme', this.checkTime())
+  }
+
 }
+
+
